fadb1a71e88e8904b331fbf5a1b10e65dd304fd3
eveng <eveng.thao@careerbuilder.com>
2016-03-17 09:28:55 -0400

    initial

---
+++ SingleToPattern/LogManager.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+
+namespace SingletonPattern
+{
+    public class LogManager
+    {
+        private static LogManager _instance;
+        private FileStream _fileStream;
+        private StreamWriter _streamWriter;
+ 
+ 
+        private LogManager()
+        {
+            _fileStream = File.OpenWrite(GetExectionFolder()  + "Application.log");
+       
+            _streamWriter = new StreamWriter(_fileStream);
+            _streamWriter.AutoFlush = true;
+        }
+
+        private string GetExectionFolder()
+        {
+            return Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+        }
+
+        public void WriteLog(string message)
+        {
+            _streamWriter.WriteLine(message);
+
+        }
+
+        public static LogManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new LogManager();
+                    
+                }
+                return _instance;
+            }
+        }
+
+  
+    }
+}
---
+++ SingleToPattern/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LogManager lg = LogManager.Instance;
+
+            lg.WriteLog("test");
+            Console.WriteLine("test");
+
+        }
+    }
+}
